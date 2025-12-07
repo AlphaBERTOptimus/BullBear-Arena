43515d42e590f4c2f7cea7f1f245f63de44aa6be
AlphaBERTOptimus <149768903+AlphaBERTOptimus@users.noreply.github.com>
2025-12-07 19:15:17 +0800

    __init__.py

--- bullbear_arena/core/__init__.py
+++ bullbear_arena/core/__init__.py
@@ -1,4 +1,4 @@
-from bullbear_arena.core.question_router import QuestionRouter
-from bullbear_arena.core.flexible_executor import FlexibleExecutor
+from .bullbear_arena.core.question_router import QuestionRouter
+from .bullbear_arena.core.flexible_executor import FlexibleExecutor
 
 __all__ = ["QuestionRouter", "FlexibleExecutor"]
